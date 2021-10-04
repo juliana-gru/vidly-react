081d11ec2f8a0769c39355cca74491183dde2886
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2021-10-04 15:35:29 -0400

    add validation to login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,22 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Joi from 'joi-browser';
+import Input from './common/Input';
+
 
 const LoginForm = () => {
-  const [state, setState] = React.useState({
-    username: "",
+  const [account, setAccount] = React.useState({
+    username: "", //need to initialize with empty string because value can't be null or undefined
     password: ""
-  })
+  });
+  const [errors, setErrors] = React.useState({});
 
-  const handleChange = ({ currentTarget: input }) => {
-    console.log(input.name);
+  const schema = {
+    username: Joi.string().required().label('Username'),
+    password: Joi.string().required().label('Password')
+  };
+
+  const validate = () => {
+    const options = { abortEarly: false }
+    const { error }= Joi.validate(account, schema, options);
+    // console.log(result);
+
+    if (!error) return null;
+
+    const errors = {};
+    for (let item of error.details) 
+      errors[item.path[0]] = item.message; //could use map as well
+    
+    return errors;    
+  }
+
+  const validateProperty = ({ name, value }) => {    
+    const obj = { [name]: value };
+    const propertySchema = { [name]: schema[name]}
+    const { error } = Joi.validate(obj, propertySchema);
+    return error ? error.details[0].message: null;
+  }
+
+  const handleChange = ({ currentTarget: input }) => {   
+    const errorMessage = validateProperty(input);  
+    
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const value = input.value;
-    setState({
-      ...state,
+    setAccount({
+      ...account,
       [input.name]: value
     });
   }
   
   const handleSubmit = e => {
     e.preventDefault();
+
+    const errors = validate();    
+    setErrors(errors || {});
+    
+    if (errors) return;
 
     // call the server
     console.log('submitted');
@@ -26,27 +65,22 @@ const LoginForm = () => {
     <div>
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input 
-            value={state.username} 
-            onChange={handleChange} 
-            autoFocus 
-            name="username" 
-            id="username" 
-            type="text" 
-            className="form-control" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input value={state.password} 
-            onChange={handleChange} 
-            name="password" 
-            id="password" 
-            type="text" 
-            className="form-control" />
-        </div>
-        <button className="btn btn-primary">Login</button>
+        <Input 
+          name="username" 
+          value={account.username} 
+          label="Username"
+          onChange={handleChange} 
+          error={errors.username}
+        />
+        <Input 
+          name="password"
+          type="password"
+          value={account.password}
+          label="Password"
+          onChange={handleChange}
+          error={errors.password}
+        />
+        <button disabled={validate()} className="btn btn-primary">Login</button>
       </form>
     </div>
   );
